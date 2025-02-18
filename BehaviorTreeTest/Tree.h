68fac7f4bb9b7f86ba3e1dc119030d51ec8b8124
Lavigne <quentin.lavigne@thermofisher.com>
2025-02-18 20:05:14 +0100

    [WIP]

--- BehaviorTreeTest/Tree.h
+++ BehaviorTreeTest/Tree.h
@@ -101,6 +101,114 @@ namespace BT
 
     };
     
+    class ControlNode;
+
+    class IStrategy
+    {
+    public:
+        virtual ~IStrategy() = default;
+        virtual Status execute(ControlNode* node) = 0;
+    };
+
+    class ControlNode : public CompositeNode
+    {
+    public:
+        ControlNode(CompositeNode* parent, IStrategy* failedStrategy, IStrategy* runningStrategy, IStrategy* successStrategy)
+            : CompositeNode(parent)
+            , m_nextNodeToTick(0)
+            , m_failedStrategy(failedStrategy)
+            , m_runningStrategy(runningStrategy)
+            , m_successStrategy(successStrategy)
+        {
+        }
+
+        ~ControlNode()
+        {
+            delete m_failedStrategy;
+            delete m_runningStrategy;
+            delete m_successStrategy;
+        }
+
+        void reset()
+        {
+            m_nextNodeToTick = 0;
+        }
+
+        void incrementChildrenIndex()
+        {
+            m_nextNodeToTick++;
+        }
+
+        bool isLastChildren() const
+        {
+            return m_nextNodeToTick == getChildren().size() - 1;
+        }
+
+        virtual Status tick()
+        {
+            Status childStatus = getChildren()[m_nextNodeToTick]->tick();
+
+            if (childStatus == Failed)
+                return m_failedStrategy->execute(this);
+
+            if (childStatus == Running)
+                return m_runningStrategy->execute(this);
+
+            if (childStatus == Success)
+                return m_successStrategy->execute(this);
+
+            throw;
+        }
+
+    private:
+        size_t m_nextNodeToTick;
+        IStrategy* m_failedStrategy;
+        IStrategy* m_runningStrategy;
+        IStrategy* m_successStrategy;
+    };
+
+    struct FailedStrategy : public IStrategy
+    {
+        virtual Status execute(ControlNode* node)
+        {
+            node->reset();
+            return Failed;
+        }
+    };
+
+    struct RetryStrategy : public IStrategy
+    {
+        virtual Status execute(ControlNode* node)
+        {
+            node->reset();
+            return Running;
+        }
+    };
+
+    struct RunningStrategy : public IStrategy
+    {
+        virtual Status execute(ControlNode* node)
+        {
+            return Running;
+        }
+    };
+
+    struct SuccessStrategy : public IStrategy
+    {
+        virtual Status execute(ControlNode* node)
+        {
+            if (node->isLastChildren())
+            {
+                node->reset();
+                return Success;
+            }
+            
+            node->incrementChildrenIndex();
+            return Running;
+        }
+    };
+
+    /*
     class Sequence : public CompositeNode
     {
     public:
@@ -135,7 +243,26 @@ namespace BT
     private:
         size_t m_nextNodeToTick;
     };
+    */
 
+    class Sequence : public ControlNode
+    {
+    public:
+        Sequence(CompositeNode* parent = nullptr) :
+            ControlNode(parent, new FailedStrategy, new RunningStrategy, new SuccessStrategy)
+        {}
+    };
+
+    class Retry : public ControlNode
+    {
+    public:
+        Retry(CompositeNode* parent = nullptr) :
+            ControlNode(parent, new RetryStrategy, new RunningStrategy, new SuccessStrategy)
+        {
+        }
+    };
+
+    /*
     class Retry : public CompositeNode
     {
     public:
@@ -169,7 +296,7 @@ namespace BT
     private:
         size_t m_nextNodeToTick;
     };
-
+    */
     class ReloadGunIfEmpty : public ActionNode
     {
     public:
