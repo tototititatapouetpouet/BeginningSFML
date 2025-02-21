09638a0924b316f87cf8a5762a70ac632e713608
Lavigne <quentin.lavigne@thermofisher.com>
2025-02-21 18:55:12 +0100

    EXO fix2

--- BehaviorTreeTest/Dog.h
+++ BehaviorTreeTest/Dog.h
@@ -17,11 +17,20 @@ public:
     void bark();
 };
 
+//  First version of the behavior:
+//
 //  Eat every food available.
 //      -> locate food
 //      -> go to food
 //      -> eat food
-//  One there is no food left, bark for more of it!
+//  Once there is no food left, bark for more of it!
+//
+//
+// 
+//  Second version of the behavior:
+//
+//  Now the dog should exhibit a 'food eaten' capacity that is increased by 1 each time doggy eats some food.
+//  When this food eaten capacity reaches a given threshold, doggy need to poop() before being able to eat again!
 
 class Game2
 {
